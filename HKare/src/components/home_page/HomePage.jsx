9ad36112e23fdde33ff26113b0293da22240616b
Umesh J <umeshbtech05@gmail.com>
2025-01-30 16:05:04 +0530

    Updated the HomePage

--- HKare/src/components/home_page/HomePage.jsx
+++ HKare/src/components/home_page/HomePage.jsx
@@ -3,18 +3,33 @@ import "./HomePage.css";
 
 const HomePage = () => {
   return (
-    <div className="home-container">
-      <div className="login-buttons">
-        <div className="DOCTOR">
-          <a href="#">DOCTOR</a>
-        </div>
-        <div className="PATIENT">
-          <a href="#">PATIENT</a>
-        </div>
-        <div className="STAFF">
-          <a href="#">STAFF</a>
+    <div className="homepage-wrapper">
+      <header className="header">
+        <img src="/path/to/logo.png" alt="Hospital Logo" className="logo" />
+      </header>
+
+      <div className="home-container">
+        <div className="login-buttons">
+          <div className="DOCTOR">
+            <img src="doctor-image.png" alt="Doctor" className="card-image" />
+            <div className="label">DOCTOR</div>
+          </div>
+
+          <div className="PATIENT">
+            <img src="patient-image.png" alt="Patient" className="card-image" />
+            <div className="label">PATIENT</div>
+          </div>
+
+          <div className="STAFF">
+            <img src="staff-image.png" alt="Staff" className="card-image" />
+            <div className="label">STAFF</div>
+          </div>
         </div>
       </div>
+
+      <footer className="footer">
+        <h1>Copyright 2025 HKare</h1>
+      </footer>
     </div>
   );
 };
